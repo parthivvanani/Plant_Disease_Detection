289575db2bc468d10986e7fa5e87ccd2c29adb47
parthivvanani <parthiv.vanani222@gmail.com>
2021-05-13 15:13:55 -0400

    static

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.htm'));
+});
+
+app.get("/types", (req, res) => {
+  res.sendFile(path.join(__dirname, './static/types.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}!`);
+});
